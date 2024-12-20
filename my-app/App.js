f3a1da84706df13b197038df7b62f78b1729c036
TrungNgT <trungnguyenthanh.eworking@gmail.com>
2024-12-20 09:20:31 +0700

    change the frontend template

--- my-app/App.js
+++ my-app/App.js
@@ -1,27 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {  Platform } from 'react-native';
+import LoginScreen from './login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>Hello from HUST! </Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'skyblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textStyle: {
-    fontStyle: 'italic',
-    fontSize: Platform.OS === 'android' ? 30 : 50,
-    color: Platform.OS === 'android' ? 'yellow' : 'red',
-    justifyContent: 'center'
-  }
-});
