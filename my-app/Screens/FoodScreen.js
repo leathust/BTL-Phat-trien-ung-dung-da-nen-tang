d3eca7c6e53ff4323d210c28cf720bb9970157bb
TrungNgT <trungnguyenthanh.eworking@gmail.com>
2024-12-31 12:43:21 +0700

    commit

--- my-app/Screens/FoodScreen.js
+++ my-app/Screens/FoodScreen.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-
-import { ShopProvider } from '../Context/ShopContext';
-
 import FoodListScreen from './FoodList';
-import FoodFindScreen from './FoodFindScreen';
-import FoodReportScreen from './FooodReportScreen';
 
 const Drawer = createDrawerNavigator();
 
 
 const FoodScreen = ({ navigation }) => {
   return (
-    <ShopProvider>
       <Drawer.Navigator initialRouteName="Quản lý thực phẩm">
         <Drawer.Screen name="Quản lý thực phẩm" component={FoodListScreen} />
         {/* <Drawer.Screen name="Tìm kiếm" component={FoodFindScreen} />
         <Drawer.Screen name="Thống kê" component={FoodReportScreen} /> */}
       </Drawer.Navigator>
-    </ShopProvider>
   );
 };
 
